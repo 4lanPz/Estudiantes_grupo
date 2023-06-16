6bc314375c76a7b13aebc5fe9296015289ebc17d
cristian-simba <davidsimba200@gmail.com>
2023-06-16 11:42:32 -0500

    Estudiante

--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,2 +1,45 @@
 public class Estudiantes {
+     String nombre;
+     String apellido;
+     String institucion;
+     String cedula;
+
+    public Estudiantes(String nombre, String apellido, String institucion, String cedula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.institucion = institucion;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
 }
