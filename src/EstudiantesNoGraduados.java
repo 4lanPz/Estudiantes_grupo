6cdbae8525b1edcd391ef51968409b8b379b86a8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-06-16 11:50:49 -0500

    Constructor Clase Hija EstudianteNoGraduados

--- src/EstudiantesNoGraduados.java
+++ src/EstudiantesNoGraduados.java
@@ -1,2 +1,35 @@
-public class EstudiantesNoGraduados {
+public class EstudiantesNoGraduados extends Estudiantes {
+
+    //ATRIBUTOS
+    private String periodo;
+    private String nueva_fecha_prueba;
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getNueva_fecha_prueba() {
+        return nueva_fecha_prueba;
+    }
+
+    public void setNueva_fecha_prueba(String nueva_fecha_prueba) {
+        this.nueva_fecha_prueba = nueva_fecha_prueba;
+    }
+
+    public EstudiantesNoGraduados(String nombre,
+                                  String apellido,
+                                  String institucion,
+                                  String cedula,
+                                  String periodo,
+                                  String nueva_fecha_prueba) {
+        super(nombre, apellido, institucion, cedula);
+        this.periodo=periodo;
+        this.nueva_fecha_prueba=nueva_fecha_prueba;
+    }
+
+
 }
